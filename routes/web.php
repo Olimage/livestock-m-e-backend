ce640f3d375fcdaa028c13a4f7904abb6987225c
David Inyang <davidinyang01@gmail.com>
2025-10-13 14:25:51 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Inertia\Inertia;
 Route::get('/', function () {
     // return view('welcome');
 
-     return Inertia::render('Welcome');
+    //  return Inertia::render('Welcome');
     //  return Inertia::render('Welcome', [
     //     'canLogin' => Route::has('login'),
     //     'canRegister' => Route::has('register'),
